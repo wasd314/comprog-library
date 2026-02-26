a5bc6a1f54e44fe00c534cb6e03d383117ee5f4c
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-02-27 01:05:49 +0900

    forward declaration

---
+++ include/comprog/fps/fps-declare.hpp
@@ -0,0 +1,11 @@
+#ifndef WASD314_COMPROG_FPS_FPS_DECLARE_HPP_
+#define WASD314_COMPROG_FPS_FPS_DECLARE_HPP_
+
+namespace comprog::fps {
+template <typename T>
+struct Fps;
+template <typename T>
+struct Freq;
+}  // namespace comprog::fps
+
+#endif  // WASD314_COMPROG_FPS_FPS_DECLARE_HPP_
