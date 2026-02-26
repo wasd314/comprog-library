98d48054c5d88a7ba9aff4e12e40b448b10c26bd
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-02-27 03:47:03 +0900

    tag dispatch

---
+++ include/comprog/fps/dispatch-tag.hpp
@@ -0,0 +1,13 @@
+#ifndef WASD314_COMPROG_FPS_DISPATCH_TAG_HPP_
+#define WASD314_COMPROG_FPS_DISPATCH_TAG_HPP_
+
+namespace comprog::fps {
+struct Auto {};
+struct Sparse {};
+struct Dense {};
+struct DenseNtt {};
+
+namespace impl {}
+}  // namespace comprog::fps
+
+#endif  // WASD314_COMPROG_FPS_DISPATCH_TAG_HPP_
