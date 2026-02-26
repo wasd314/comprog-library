d3359cd45673f82be285c311b25f75c9cfa75737
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-02-26 15:45:42 +0900

    type alias

---
+++ library/utility/types.hpp
@@ -0,0 +1,11 @@
+#ifndef WASD314_LIBRARY_UTILITY_TYPES_HPP_
+#define WASD314_LIBRARY_UTILITY_TYPES_HPP_
+
+namespace wasd314 {
+using i64 = long long;
+using u64 = unsigned long long;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+}  // namespace wasd314
+
+#endif  // WASD314_LIBRARY_UTILITY_TYPES_HPP_
